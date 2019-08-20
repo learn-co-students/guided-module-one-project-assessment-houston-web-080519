583fa24d0b662558a581f58ab3937a05c99cf4f0
Emily <mle.jo.11@gmail.com>
2019-08-20 15:34:47 -0500

    made menu, drafting responses

--- lib/index.rb
+++ lib/index.rb
@@ -13,6 +13,18 @@ while true
         "Last Weekend"])
     
 if users_response == "Coding"
+    puts adj1.partofspeech
+    users_response = gets.chomp
+    UserInput.create({
+        order_id: adj1.order_id
+        input: users_response
+    })
+    #I want them to be prompted with the first part of speech, id 101, 
+    #for story id 1. user inputs something which is pushed into the 
+    #first blank of story 1. They are prompted with PoSpeech id 102, etc.
+    #after the final PoSpeech prompt for story id 1, they are returned
+    #the completed story 1 with their inputs interpolated into the 
+    #blanks.
 end
 
 if users_response == "Last Weekend"
