e51b2b8d118d4ffbdc040e6be2fb4b1dc764a05b
Emily <mle.jo.11@gmail.com>
2019-08-21 13:05:05 -0500

    working madlib methods, but returns each word in madlib as well

---
+++ bin/index.rb
@@ -0,0 +1,121 @@
+require_relative '../config/environment.rb'
+ActiveRecord::Base.logger = nil
+prompt = TTY::Prompt.new
+
+quit_game = false
+
+while !quit_game
+    users_response = prompt.select("Hello! Welcome to MadLibs! Which story would you like to play?",
+        ["Coding",
+        "Last Weekend"])
+
+        if users_response == "Coding"
+            return_story = []
+            mad_lib_id = 1
+            current_mad_lib = MadLib.all.find do |mad_lib|
+                mad_lib.story_id == mad_lib_id
+            end
+
+            mad_lib_array = current_mad_lib.story.split(" ")
+            mad_lib_array.each do |word|
+            # if we hit a special character we prompt the user
+                if word == "_"
+                    #function prompt
+                        puts "Give me a word."
+                        temp_word = gets.chomp
+                    
+                        word = temp_word
+                end
+                puts "we are shoveling in #{word}"
+                return_story << word
+            # if we hit the end of the array, we read the madlib
+            end
+            
+            # binding.pry
+            puts return_story.join(" ")
+            puts "hit enter to continue..."
+            gets
+    
+    # if users_response == "Coding"
+    #     while true do
+    #     counter = 101
+    #     storyarray = []
+    #     compstoryarray = []
+    #     puts PartsOfSpeech.partofspeech WHEN order_id = counter
+    #     users_response1 = gets.chomp
+    #     users_response1 << storyarray
+    #     UserInput.create({
+    #         order_id: adj1.order_id,
+    #         input: users_response1
+    #     })
+        
+
+    #     counter += 1 
+    #     end#while
+        # for running next part of speech? make a loop,
+        #push each response into story array until it hits part of speech
+        #id 111.
+
+        # runs through each part of speech in order beginning with part of 
+        # speech 101, ending with 111. push each response into storyarray.
+        # after 111, puts out story 1 with user inputs.
+
+
+
+        #I want them to be prompted with the first part of speech, id 101, 
+        #for story id 1. user inputs something which is pushed into the 
+        #first blank of story 1. They are prompted with PoSpeech id 102, etc.
+        #after the final PoSpeech prompt for story id 1, they are returned
+        #the completed story 1 with their inputs interpolated into the 
+        #blanks.
+    end#coding
+
+
+# if users_response == "Last Weekend"
+# end
+    if users_response == "Last Weekend"
+        return_story = []
+        mad_lib_id = 2
+        # get the story strinhg
+        current_mad_lib = MadLib.all.find do |mad_lib|
+            mad_lib.story_id == mad_lib_id
+        end
+
+        
+        # split the story string into an array
+        # binding.pry
+        mad_lib_array = current_mad_lib.story.split(" ")
+        # iterate through the story array 
+        mad_lib_array.each do |word|
+        # if we hit a special character we prompt the user
+            if word == "_"
+                #function prompt
+                    puts "Give me a word."
+                    temp_word = gets.chomp
+                
+                    word = temp_word
+            end
+            puts "we are shoveling in #{word}"
+            return_story << word
+        # if we hit the end of the array, we read the madlib
+        end
+        
+        # binding.pry
+        puts return_story.join(" ")
+        puts "hit enter to continue..."
+        gets
+
+        exit
+
+    end#Last Weekend
+
+    # def replace
+    #     puts "Give me a word."
+    #     gets.chomp
+    #     #prompt the user
+    #     #return the user input
+    # end
+end
+
+
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,18 +2,23 @@ MadLib.destroy_all
 PartsOfSpeech.destroy_all
 UserInput.destroy_all
 
+puts "adding story 1"
 story1 = MadLib.create(story_id: 1, story: "Coding gets a bad rap as _, but it’s actually very _. People use coding _, for things like browsing reddit to playing _. For example, if you want a computer to _ your _, you can create your own _. Coding can be very _, but remember- it’s not about the _, it’s about the _! I love _!")
-adj1 = PartsOfSpeech.create(order_id: 101, story_id: story1.story_id, part_of_speech: "Adjective")
-adj2 = PartsOfSpeech.create(order_id: 102, story_id: story1.story_id, part_of_speech: "Adjective")
-fot = PartsOfSpeech.create(order_id: 103, story_id: story1.story_id, part_of_speech: "Frequency of Time")
-game = PartsOfSpeech.create(order_id: 104, story_id: story1.story_id, part_of_speech: "A Game")
-ptv = PartsOfSpeech.create(order_id: 105, story_id: story1.story_id, part_of_speech: "Present Tense Verb")
-noun1 = PartsOfSpeech.create(order_id: 106, story_id: story1.story_id, part_of_speech: "Noun")
-noun2 = PartsOfSpeech.create(order_id: 107, story_id: story1.story_id, part_of_speech: "Noun")
-adj3 = PartsOfSpeech.create(order_id: 108, story_id: story1.story_id, part_of_speech: "Adjective")
-noun3 = PartsOfSpeech.create(order_id: 109, story_id: story1.story_id, part_of_speech: "Noun")
-noun4 = PartsOfSpeech.create(order_id: 110, story_id: story1.story_id, part_of_speech: "Noun")
-ving = PartsOfSpeech.create(order_id: 111, story_id: story1.story_id, part_of_speech: "Verb + ing")
+# adj1 = PartsOfSpeech.create(order_id: 101, story_id: story1.story_id, part_of_speech: "Adjective")
+# adj2 = PartsOfSpeech.create(order_id: 102, story_id: story1.story_id, part_of_speech: "Adjective")
+# fot = PartsOfSpeech.create(order_id: 103, story_id: story1.story_id, part_of_speech: "Frequency of Time")
+# game = PartsOfSpeech.create(order_id: 104, story_id: story1.story_id, part_of_speech: "A Game")
+# ptv = PartsOfSpeech.create(order_id: 105, story_id: story1.story_id, part_of_speech: "Present Tense Verb")
+# noun1 = PartsOfSpeech.create(order_id: 106, story_id: story1.story_id, part_of_speech: "Noun")
+# noun2 = PartsOfSpeech.create(order_id: 107, story_id: story1.story_id, part_of_speech: "Noun")
+# adj3 = PartsOfSpeech.create(order_id: 108, story_id: story1.story_id, part_of_speech: "Adjective")
+# noun3 = PartsOfSpeech.create(order_id: 109, story_id: story1.story_id, part_of_speech: "Noun")
+# noun4 = PartsOfSpeech.create(order_id: 110, story_id: story1.story_id, part_of_speech: "Noun")
+# ving = PartsOfSpeech.create(order_id: 111, story_id: story1.story_id, part_of_speech: "Verb + ing")
 
-input1 = UserInput.create(order_id: adj1.order_id, input: "cute")
-puts PartsOfSpeech.partofspeech WHEN order_id = 110
+puts "adding story 2"
+story2 = MadLib.create(story_id: 2, story: "Have you ever wished you could _ a(n) _ ? Last weekend, I did just that. I went to _ on my _ . On the way, I stopped for _ at a(n) _ place called _ . If you have never been there, I highly recommend it. My favorite dish there is the _ . Afterwards, I was super full, but I went to _ and climbed to the top of the _ . What a view! We should all go there _ .")
+# input1 = UserInput.create(order_id: adj1.order_id, input: "cute")
+# puts PartsOfSpeech.partofspeech WHEN order_id = 110
+
+puts "seeding COMPLETE!"
